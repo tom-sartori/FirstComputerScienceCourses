439511f7723a3a7cc41669b4defb103288c23501
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2020-09-12 22:48:21 +0200

    up

---
+++ A2/Modelisation mathématique/Sprint1/droites.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 22:46:47 2020
+
+@author: tom
+"""
+
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+x = [0.5, 1.5, 1.5, 0.5, 0.5, 1.5, 0.5, 1.5]
+y = [0.5, 0.5, 2, 2, 0.5, 2, 2, 0.5]
+plt.plot(x,y) # tracé de la ligne
